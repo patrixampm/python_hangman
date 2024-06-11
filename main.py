a3a222f5b1fbd22a16811da67da510e1e1782e3c
patrixampm <patrick.a.peckham@gmail.com>
2024-06-11 12:05:27 +0200

    hangman game

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+import hangman_art
+import hangman_words
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"HEY! You have already chosen {guess}, please choose another letter")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        print(f"WRONG!!! {guess} is not in this word")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print(f"You lose. The word was {chosen_word}")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
